0f3c8f0c89d1730ebadcfea35bbe1c5fa747d62b
naihaisky <abc@zhfsky.com>
2017-06-12 16:24:51 +0800

    增加安装检查

--- index.php
+++ index.php
@@ -11,14 +11,28 @@
 
 // 应用入口文件
 
+// 检测是否安装应用
+if(file_exists("./Public/install") && !file_exists("./Public/install/install.lock")){
+	// 组装安装url
+	$url=$_SERVER['HTTP_HOST'].trim($_SERVER['SCRIPT_NAME'],'index.php').'Public/install/index.php';
+	header("Location:https://$url");die;
+}
+
+
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+define('APP_DEBUG',true);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
+
+//定义工作路径
+define('WORKING_PATH',str_replace('\\','/',__DIR__));
+//定义上传根目录
+define('UPLOAD_ROOT_PATH','/Public/Upload/');
+
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
